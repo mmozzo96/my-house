19034d76f3ba8cc08a2bb668ef74562476053cde
Mozzo Michele <michele.mozzo@kone.com>
2023-07-19 20:31:57 +0300

    book element

--- src/components/Visualizer/elements/book.ts
+++ src/components/Visualizer/elements/book.ts
@@ -3,55 +3,128 @@ import { Element3D, ElementType } from "./element";
 import * as THREE from "three";
 
 export class Book extends Element3D implements ElementType {
-  constructor(vpt: Viewport) {
-    super(vpt);
-    this.CreateElement();
-
-    this.ScaleForDebugging(5);
-  }
-
-  public CreateElement() {
-    const width = 1;
-    const height = 0.15;
-    const depth = 0.65;
-    const geometry = new THREE.BoxGeometry(width, height, depth, 20, 100, 20);
-
-    const vertices = geometry.attributes.position;
-    const v3 = new THREE.Vector3();
-    for (let i = 0; i < vertices.count; i++) {
-      v3.fromBufferAttribute(vertices, i);
-      if (v3.z > depth / 2 - 0.03 && Math.abs(v3.y) < height / 2 - 0.01) {
-        v3.z = depth / 2 - 0.03;
-      }
-      if (
-        v3.x > width / 2 - 0.02 &&
-        Math.abs(v3.y) < height / 2 - 0.01 &&
-        v3.z > -depth / 2 + 0.00001
-      ) {
-        v3.x = width / 2 - 0.02;
-      }
-      if (
-        v3.x < -width / 2 + 0.02 &&
-        Math.abs(v3.y) < height / 2 - 0.01 &&
-        v3.z > -depth / 2 + 0.00001
-      ) {
-        v3.x = -width / 2 + 0.02;
-      }
-      if (v3.z < -depth / 2 + 0.00001) {
-        v3.z =
-          -(((height / 2) ** 2 - (Math.abs(v3.y) - 0.001) ** 2) ** (1 / 2)) -
-          depth / 2;
-      }
-      vertices.setX(i, v3.x);
-      vertices.setZ(i, v3.z);
+    constructor(vpt: Viewport) {
+        super(vpt);
+        this.CreateElement();
     }
 
-    const mesh = new THREE.Mesh(geometry, this.vpt.material);
+    public CreateElement() {
+        const width = 1;
+        const height = 0.15;
+        const depth = 0.65;
+        const geometry = new THREE.BoxGeometry(
+            width,
+            height,
+            depth,
+            20,
+            100,
+            20
+        );
 
-    this.element.add(mesh);
-  }
+        const vertices = geometry.attributes.position;
+        const v3 = new THREE.Vector3();
+        for (let i = 0; i < vertices.count; i++) {
+            v3.fromBufferAttribute(vertices, i);
+            if (v3.z > depth / 2 - 0.03 && Math.abs(v3.y) < height / 2 - 0.01) {
+                v3.z = 0;
+            }
+            if (
+                v3.x > width / 2 - 0.02 &&
+                Math.abs(v3.y) < height / 2 - 0.01 &&
+                v3.z > -depth / 2 + 0.00001
+            ) {
+                v3.x = 0;
+            }
+            if (
+                v3.x < -width / 2 + 0.02 &&
+                Math.abs(v3.y) < height / 2 - 0.01 &&
+                v3.z > -depth / 2 + 0.0001
+            ) {
+                v3.x = 0;
+            }
+            if (v3.z < -depth / 2 + 0.00001) {
+                v3.z =
+                    -(
+                        ((height / 2) ** 2 - (Math.abs(v3.y) - 0.001) ** 2) **
+                        (1 / 2)
+                    ) -
+                    depth / 2;
+            }
+            vertices.setX(i, v3.x);
+            vertices.setZ(i, v3.z);
+        }
 
-  private ScaleForDebugging(scale: number) {
-    this.element.scale.set(scale, scale, scale);
-  }
+        const material = this.vpt.material.clone();
+        material.color = new THREE.Color(0x00ff00);
+        material.opacity = 1;
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.name = "cover";
+
+        this.element.add(mesh);
+
+        const pageMaterial = this.vpt.material.clone();
+        pageMaterial.color = new THREE.Color(0xffffff);
+        pageMaterial.opacity = 1;
+
+        const page1Geometry = new THREE.PlaneGeometry(
+            width - 0.04,
+            height - 0.02
+        );
+        page1Geometry.translate(0, 0, depth / 2 - 0.0299);
+        const page1 = new THREE.Mesh(page1Geometry, pageMaterial);
+
+        this.element.add(page1);
+
+        const page2Geometry = new THREE.PlaneGeometry(
+            height / 2 + depth - 0.03,
+            height - 0.02,
+            1000,
+            100
+        );
+        page2Geometry.rotateY(Math.PI / 2);
+        page2Geometry.translate(width / 2 - 0.0199, 0, -(height / 4 + 0.015));
+        const p2Vertices = page2Geometry.attributes.position;
+        const p2V3 = new THREE.Vector3();
+        for (let i = 0; i < p2Vertices.count; i++) {
+            p2V3.fromBufferAttribute(p2Vertices, i);
+            if (
+                p2V3.z < -depth / 2 &&
+                (p2V3.z + depth / 2) ** 2 + p2V3.y ** 2 > (height / 2) ** 2
+            ) {
+                p2V3.z =
+                    -(
+                        ((height / 2) ** 2 - (Math.abs(v3.y) - 0.001) ** 2) **
+                        (1 / 2)
+                    ) -
+                    depth / 2;
+            }
+            p2Vertices.setZ(i, p2V3.z);
+        }
+        const page2 = new THREE.Mesh(page2Geometry, pageMaterial);
+
+        this.element.add(page2);
+
+        const page3Geometry = page2Geometry.clone();
+        page3Geometry.rotateZ(Math.PI);
+        const page3 = new THREE.Mesh(page3Geometry, pageMaterial);
+
+        this.element.add(page3);
+
+        this.element.position.set(0, height / 2, 0);
+
+        this.width = width;
+        this.height = height;
+        this.depth = depth + height / 2;
+    }
+
+    public SetColor(color: THREE.Color) {
+        const cover = this.element.children.find(
+            (o) => o.name === "cover"
+        ) as THREE.Mesh<THREE.BufferGeometry, THREE.MeshPhysicalMaterial>;
+        cover.material.color = color;
+    }
+
+    private ScaleForDebugging(scale: number) {
+        this.element.scale.set(scale, scale, scale);
+    }
 }
--- src/components/Visualizer/visualization.ts
+++ src/components/Visualizer/visualization.ts
@@ -7,39 +7,57 @@ import { Chair } from "./elements/chair";
 import { Book } from "./elements/book";
 
 export class Visualization {
-  vpt: Viewport;
+    vpt: Viewport;
 
-  constructor(vpt: Viewport) {
-    this.vpt = vpt;
-  }
+    constructor(vpt: Viewport) {
+        this.vpt = vpt;
+    }
 
-  public AddElements() {
-    const assets = [];
+    public AddElements() {
+        const assets: THREE.Group[] = [];
 
-    const table = new Desk(this.vpt);
+        const table = new Desk(this.vpt);
 
-    const screen = new Screen(this.vpt);
-    screen.element.position.add(
-      new THREE.Vector3(0, table.height, -table.depth / 6)
-    );
+        const screen1 = new Screen(this.vpt);
+        screen1.element.rotateY((15 * Math.PI) / 180);
+        screen1.element.position.add(
+            new THREE.Vector3(-table.width / 10, table.height, -table.depth / 5)
+        );
 
-    const keyboard = new Keyboard(this.vpt);
-    keyboard.element.position.add(
-      new THREE.Vector3(0, table.height, table.depth / 4)
-    );
+        const screen2 = new Screen(this.vpt);
+        screen2.element.rotateY(-(25 * Math.PI) / 180);
+        screen2.element.position.add(
+            new THREE.Vector3(table.width / 4, table.height, -table.depth / 7)
+        );
 
-    const chair = new Chair(this.vpt);
-    chair.element.rotateY(Math.PI);
-    chair.element.position.add(new THREE.Vector3(0, 0, table.depth / 2));
+        const keyboard = new Keyboard(this.vpt);
+        keyboard.element.position.add(
+            new THREE.Vector3(table.width / 15, table.height, table.depth / 4)
+        );
 
-    const book = new Book(this.vpt);
+        const chair = new Chair(this.vpt);
+        chair.element.rotateY(Math.PI);
+        chair.element.position.add(
+            new THREE.Vector3(table.width / 15, 0, table.depth / 2)
+        );
 
-    /* assets.push(table.element);
-    assets.push(screen.element);
-    assets.push(keyboard.element);
-    assets.push(chair.element); */
-    assets.push(book.element);
+        const book = new Book(this.vpt);
+        book.element.rotateY((70 * Math.PI) / 180);
+        book.element.position.add(
+            new THREE.Vector3(
+                (-2 * table.width) / 6,
+                table.height,
+                -table.depth / 8
+            )
+        );
 
-    assets.forEach((element) => this.vpt.scene.add(element));
-  }
+        assets.push(table.element);
+        assets.push(screen1.element);
+        assets.push(screen2.element);
+        assets.push(keyboard.element);
+        assets.push(chair.element);
+        assets.push(book.element);
+
+        assets.forEach((element) => this.vpt.scene.add(element));
+    }
 }
